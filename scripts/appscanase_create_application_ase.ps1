9b4b066ca38cf5f95191513157dc91fc390163d4
jrocia <69405400+jrocia@users.noreply.github.com>
2023-05-15 09:36:27 -0300

    Create appscanase_create_application_ase.ps1

---
+++ scripts/appscanase_create_application_ase.ps1
@@ -0,0 +1,32 @@
+# Copyright 2023 HCL America
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+write-host "======== Step: Checking application in AppScan Enterprise ========"
+# ASE authentication
+$sessionId=$(Invoke-WebRequest -Method "POST" -Headers @{"Accept"="application/json"} -ContentType 'application/json' -Body "{`"keyId`": `"$env:aseApiKeyId`",`"keySecret`": `"$env:aseApiKeySecret`"}" -Uri "https://$env:aseHostname`:9443/ase/api/keylogin/apikeylogin" -SkipCertificateCheck | Select-Object -Expand Content | ConvertFrom-Json | select -ExpandProperty sessionId);
+# Looking for $aseAppName into ASE
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
+$session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$env:aseHostname")));
+$aseAppId=$(Invoke-WebRequest -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -Uri "https://$env:aseHostname`:9443/ase/api/applications/search?searchTerm=$env:aseAppName" -SkipCertificateCheck | ConvertFrom-Json).id;
+# If $env:aseAppName is Null create the application into ASE else just get the aseAppId
+if ([string]::IsNullOrWhitespace($aseAppId)){
+	$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession;
+	$session.Cookies.Add((New-Object System.Net.Cookie("asc_session_id", "$sessionId", "/", "$env:aseHostname")));
+	$aseAppId=$(Invoke-WebRequest -Method POST -WebSession $session -Headers @{"Asc_xsrf_token"="$sessionId"} -ContentType "application/json" -Body "{`"name`":`"$env:aseAppName`" }" -Uri "https://$env:aseHostname`:9443/ase/api/applications" -SkipCertificateCheck | ConvertFrom-Json).id;
+	echo "$aseAppId" > aseAppId.txt
+	write-host "Application $env:aseAppName registered with id $aseAppId"
+    }
+else{
+	write-host "There is a registered application."
+	}
